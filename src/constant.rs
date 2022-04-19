2985e507dcc14a98f03efabdba78de3ce921bf90
habib <hassan.habib@epfl.ch>
2022-04-19 17:00:30 +0200

    put constant value in a different file

---
+++ src/constant.rs
@@ -0,0 +1,6 @@
+pub const WIDTH: usize = 64;
+pub const HEIGHT: usize = 32;
+pub const M: usize = 30;
+pub const RWIDTH: u32 = (M * WIDTH) as u32;
+pub const RHEIGHT: u32 = (M * HEIGHT) as u32;
+pub type Array = [[bool; HEIGHT]; WIDTH];
